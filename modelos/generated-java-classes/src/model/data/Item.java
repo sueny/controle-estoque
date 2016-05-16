cd3e7a8c5d12fe212245da1f3c24a622e0fc5003
phaffonso <pedrohaffonso@gmail.com>
2016-05-16 10:22:14 -0300

    Adicionando código fonte gerado via modelo de dominio

---
+++ modelos/generated-java-classes/src/model/data/Item.java
@@ -0,0 +1,54 @@
+package model.data;
+
+public class Item {
+
+	/**
+	 * 
+	 */
+	private Long id;
+	/**
+	 * 
+	 */
+	private String skuCode;
+	/**
+	 * 
+	 */
+	private String name;
+	/**
+	 * Getter of id
+	 */
+	public Long getId() {
+	 	 return id; 
+	}
+	/**
+	 * Setter of id
+	 */
+	public void setId(Long id) { 
+		 this.id = id; 
+	}
+	/**
+	 * Getter of skuCode
+	 */
+	public String getSkuCode() {
+	 	 return skuCode; 
+	}
+	/**
+	 * Setter of skuCode
+	 */
+	public void setSkuCode(String skuCode) { 
+		 this.skuCode = skuCode; 
+	}
+	/**
+	 * Getter of name
+	 */
+	public String getName() {
+	 	 return name; 
+	}
+	/**
+	 * Setter of name
+	 */
+	public void setName(String name) { 
+		 this.name = name; 
+	} 
+
+}
---
+++ modelos/generated-java-classes/src/model/data/MaterialCategory.java
@@ -0,0 +1,70 @@
+package model.data;
+
+public class MaterialCategory {
+
+	/**
+	 * 
+	 */
+	private Long id;
+	/**
+	 * 
+	 */
+	private String code;
+	/**
+	 * 
+	 */
+	private String name;
+	/**
+	 * 
+	 */
+	private String description;
+	/**
+	 * Getter of id
+	 */
+	public Long getId() {
+	 	 return id; 
+	}
+	/**
+	 * Setter of id
+	 */
+	public void setId(Long id) { 
+		 this.id = id; 
+	}
+	/**
+	 * Getter of code
+	 */
+	public String getCode() {
+	 	 return code; 
+	}
+	/**
+	 * Setter of code
+	 */
+	public void setCode(String code) { 
+		 this.code = code; 
+	}
+	/**
+	 * Getter of name
+	 */
+	public String getName() {
+	 	 return name; 
+	}
+	/**
+	 * Setter of name
+	 */
+	public void setName(String name) { 
+		 this.name = name; 
+	}
+	/**
+	 * Getter of description
+	 */
+	public String getDescription() {
+	 	 return description; 
+	}
+	/**
+	 * Setter of description
+	 */
+	public void setDescription(String description) { 
+		 this.description = description; 
+	} 
+
+}
