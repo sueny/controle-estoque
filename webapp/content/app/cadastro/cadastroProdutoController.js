21ce2b689f25142cb51642d90812e963eebb5cb8
jesus <vander.vander@gmail.com>
2016-05-21 08:29:45 -0300

    Ajustes na tela produto... interação 1 de n.

--- webapp/content/app/cadastro/cadastroProdutoController.js
+++ webapp/content/app/cadastro/cadastroProdutoController.js
@@ -14,20 +14,35 @@
     function cadastroProdutoController(){
         var self = this;
         self.isShow = false;
+        self.isShowBtnCadastrar = false;
         self.listaMateriaPrima = [];
         self.cadastrarProduto = cadastrarProduto;
         self.excluirProduto = excluirProduto;
         self.adicionarMateriaPrima = adicionarMateriaPrima;
-
+        self.alertDeleteMateriaPrima = alertDeleteMateriaPrima;
+        self.removerMateriaPrima = removerMateriaPrima;
 
 
         function adicionarMateriaPrima(materiaPrima){
             var mp = {
+                id: self.listaMateriaPrima.length,
                 name: materiaPrima.name,
-                qte: materiaPrima.qte
+                qte: materiaPrima.qte,
+                isShow: false
             }
             self.listaMateriaPrima.push(mp);
+            self.isShowBtnCadastrar = true;
         }
+        function removerMateriaPrima(id){
+            console.log(id);
+            self.listaMateriaPrima.splice(id,1);
+            if(self.listaMateriaPrima.length === 0) self.isShowBtnCadastrar = false;
+        }
+        function alertDeleteMateriaPrima(materiaPrima){
+            materiaPrima.isShow = !materiaPrima.isShow;
+        }
+
+
 
         function cadastrarProduto(produto){
 
