95df589f95a0493ac7de47b5435eaba7a2af83f0
jesus <vander.vander@gmail.com>
2016-05-21 19:05:59 -0300

    Ajustes na interação... commitado a 10 de n.

--- webapp/staticFileServer.js
+++ webapp/staticFileServer.js
@@ -2,27 +2,7 @@ var express = require('express');
 var config = require('./config.server');
 var app = express();
 
-app.use(function(req, res, next) {
-  /**
-   * Response settings
-   * @type {Object}
-   */
-  var responseSettings = {
-    "AccessControlAllowOrigin": req.headers.origin,
-    "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
-    "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
-    "AccessControlAllowCredentials": true
-  };
- 
-  /**
-   * Headers
-   */
-  res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
-  res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
-  res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
-  res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
-    next();
- });
+
 var http = require('http').Server(app);
 
 app.use(express.static(__dirname + config.clientPath));
@@ -30,7 +10,17 @@ app.use(express.static(__dirname + config.clientPath));
 app.get("*", function(req,res){
   res.sendFile(__dirname + config.viewPath);
 });
-
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  if ( req.method === 'OPTIONS' ) {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+});
 http.listen(config.port, function(err){
   if(err){
     console.log(err);
