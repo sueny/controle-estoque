abc1166e801e72c59da33c985f88052ae2048769
jesus <vander.vander@gmail.com>
2016-05-21 18:27:37 -0300

    Ajustes na interação... commitado a 7 de n.

--- webapp/staticFileServer.js
+++ webapp/staticFileServer.js
@@ -3,9 +3,21 @@ var config = require('./config.server');
 var app = express();
 
 app.use(function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- next();
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
  });
 var http = require('http').Server(app);
 
