4bc6eb4cd2120df2a06d136c31dfc58e2de6e202
jesus <vander.vander@gmail.com>
2016-05-21 18:33:23 -0300

    Ajustes na interação... commitado a 8 de n.

--- webapp/staticFileServer.js
+++ webapp/staticFileServer.js
@@ -3,21 +3,31 @@ var config = require('./config.server');
 var app = express();
 
 app.use(function(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  /**
+   * Response settings
+   * @type {Object}
+   */
+  var responseSettings = {
+    "AccessControlAllowOrigin": req.headers.origin,
+    "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+    "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+    "AccessControlAllowCredentials": true
+  };
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  /**
+   * Headers
+   */
+  res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+  res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+  res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+  res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
  });
 var http = require('http').Server(app);
 
