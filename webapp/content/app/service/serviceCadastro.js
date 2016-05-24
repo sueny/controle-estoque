d2a7a7b9bfb47733b1066af20b2b6e7b7ae2d69d
jesus <vander.vander@gmail.com>
2016-05-24 13:29:24 -0300

    Infomation.... 24052016-01

--- webapp/content/app/service/serviceCadastro.js
+++ webapp/content/app/service/serviceCadastro.js
@@ -17,7 +17,7 @@
 
         var API_ROUTE_CADASTRARMATERIALPRIMA = paths.apiUrl + 'material/cadastrar/';
         var API_ROUTE_EXCLUIRMATERIAPRIMA = paths.apiUrl + 'material/remover/';
-        var API_ROUTE_LISTARMATERIAPRIMA = 'https://httpbin.org/get';
+        var API_ROUTE_LISTARMATERIAPRIMA = paths.apiUrl + 'material/listar/';
         var API_ROUTE_CADASTRARFORNECEDOR = paths.apiUrl + 'fornecedor/cadastrar/';
         var API_ROUTE_EXCLUIRFORNECEDOR = paths.apiUrl + 'fornecedor/remover/';
         var API_ROUTE_LISTARFORNECEDOR = paths.apiUrl + 'fornecedor/listar/';
