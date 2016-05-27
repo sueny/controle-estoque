7ef421f3b137310be43f080ef75de1de67a8fd86
jesus <vander.vander@gmail.com>
2016-05-27 20:08:40 -0300

    Ajustando tela de cadastro de produto 27052016-05 Briancando com um Gráfico.

---
+++ webapp/content/app/relatorio/graficoController.js
@@ -0,0 +1,42 @@
+/**
+ * Created by jesus on 26/05/2016.
+ */
+
+
+(function(){
+
+    angular.module("app")
+        .controller("graficoController", graficoController);
+
+    function graficoController() {
+        var self = this;
+        self.graficoVenda = {};
+        self.graficoVenda.type = "BarChart";
+
+        self.consignacao = [
+            {v: "Consignação"},
+            {v: 100}
+        ];
+        self.calca = [
+            {v: "Venda"},
+            {v: 30}
+        ];
+
+        self.graficoVenda.data = {"cols": [
+            {id: "t", label: "Topping", type: "string"},
+            {id: "s", label: "Quantidade", type: "number"}
+        ], "rows": [
+            {c: self.consignacao},
+            {c: self.calca}
+        ]};
+
+
+        self.graficoVenda.options = {
+            'title': 'Grafico de Vendas'
+        };
+
+    }
+
+
+})();
+
