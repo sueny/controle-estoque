540a6832fec95b9f4ad86dfa9bc4fbc8621a15e8
Sueny <sueny.messias@gmail.com>
2016-06-03 16:56:12 -0300

    Correção pacotes de teste e erro de classe não existente

--- controle-estoque-base/src/test/java/projeto/engenharia/software/controle/estoque/testcases/TestCasesUpdateProduct.java
+++ controle-estoque-base/src/test/java/projeto/engenharia/software/controle/estoque/testcases/TestCasesUpdateProduct.java
@@ -3,60 +3,59 @@ package projeto.engenharia.software.controle.estoque.testcases;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import projeto.engenharia.software.controle.estoque.test.product.stateMachine.ProductState;
+import projeto.engenharia.software.controle.estoque.test.product.stateMachine.ProductStateMachine;
 
 // JUnit 4.3
-public class TestCasesUpdateProduct{
-	
-	public static junit.framework.Test suite(){
-		return new junit.framework.JUnit4TestAdapter(TestCasesUpdateProduct.class);
-	}
-	
-	@Test
-	public void test1()
-	{
-		ProductStateMachine oTestObject = new ProductStateMachine();
-		Boolean status5 = true;
-		assertEquals(true, (oTestObject.state == ProductState.Idle));
-		oTestObject.handleEvent("updateEvent");
-		assertEquals(true, (oTestObject.state == ProductState.Principal));
-		oTestObject.handleEvent("chooseProd");
-		assertEquals(true, (oTestObject.state == ProductState.Updating));
-		oTestObject.handleEvent("validProdEvent", status5);
-		assertEquals(true, (oTestObject.state == ProductState.Updated));
-		oTestObject.handleEvent("finaliseEvent");
-		
-	}
-	
-	@Test
-	public void test2()
-	{
-		ProductStateMachine oTestObject = new ProductStateMachine();
-		Boolean status5 = true;
-		assertEquals(true, (oTestObject.state == ProductState.Idle));
-		oTestObject.handleEvent("updateEvent");
-		assertEquals(true, (oTestObject.state == ProductState.Principal));
-		oTestObject.handleEvent("chooseProd");
-		assertEquals(true, (oTestObject.state == ProductState.Updating));
-		oTestObject.handleEvent("validProdEvent", status5);
-		assertEquals(true, (oTestObject.state == ProductState.Updated));
-		oTestObject.handleEvent("newUpdate");
-		assertEquals(true, (oTestObject.state == ProductState.Principal));
-		
-	}
-	
-	@Test
-	public void test3()
-	{
-		ProductStateMachine oTestObject = new ProductStateMachine();
-		Boolean status5 = false;
-		assertEquals(true, (oTestObject.state == ProductState.Idle));
-		oTestObject.handleEvent("updateEvent");
-		assertEquals(true, (oTestObject.state == ProductState.Principal));
-		oTestObject.handleEvent("chooseProd");
-		assertEquals(true, (oTestObject.state == ProductState.Updating));
-		oTestObject.handleEvent("validProdEvent", status5);
-		assertEquals(true, (oTestObject.state == ProductState.Principal));
-		
-	}
-	
+public class TestCasesUpdateProduct {
+
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(TestCasesUpdateProduct.class);
+    }
+
+    @Test
+    public void test1() {
+        ProductStateMachine oTestObject = new ProductStateMachine();
+        Boolean status5 = true;
+        assertEquals(true, (oTestObject.state == ProductState.Idle));
+        oTestObject.handleEvent("updateEvent");
+        assertEquals(true, (oTestObject.state == ProductState.Principal));
+        oTestObject.handleEvent("chooseProd");
+        assertEquals(true, (oTestObject.state == ProductState.Updating));
+        oTestObject.handleEvent("validProdEvent", status5);
+        assertEquals(true, (oTestObject.state == ProductState.Updated));
+        oTestObject.handleEvent("finaliseEvent");
+
+    }
+
+    @Test
+    public void test2() {
+        ProductStateMachine oTestObject = new ProductStateMachine();
+        Boolean status5 = true;
+        assertEquals(true, (oTestObject.state == ProductState.Idle));
+        oTestObject.handleEvent("updateEvent");
+        assertEquals(true, (oTestObject.state == ProductState.Principal));
+        oTestObject.handleEvent("chooseProd");
+        assertEquals(true, (oTestObject.state == ProductState.Updating));
+        oTestObject.handleEvent("validProdEvent", status5);
+        assertEquals(true, (oTestObject.state == ProductState.Updated));
+        oTestObject.handleEvent("newUpdate");
+        assertEquals(true, (oTestObject.state == ProductState.Principal));
+
+    }
+
+    @Test
+    public void test3() {
+        ProductStateMachine oTestObject = new ProductStateMachine();
+        Boolean status5 = false;
+        assertEquals(true, (oTestObject.state == ProductState.Idle));
+        oTestObject.handleEvent("updateEvent");
+        assertEquals(true, (oTestObject.state == ProductState.Principal));
+        oTestObject.handleEvent("chooseProd");
+        assertEquals(true, (oTestObject.state == ProductState.Updating));
+        oTestObject.handleEvent("validProdEvent", status5);
+        assertEquals(true, (oTestObject.state == ProductState.Principal));
+
+    }
+
 }
