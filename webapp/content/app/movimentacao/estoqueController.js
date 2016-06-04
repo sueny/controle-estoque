e75f0b2a2884d60103f4593b3784a08970f1ebbc
jesus <vander.vander@gmail.com>
2016-06-04 17:22:47 -0300

    cosignação - 04062016 - 03

--- webapp/content/app/movimentacao/estoqueController.js
+++ webapp/content/app/movimentacao/estoqueController.js
@@ -13,6 +13,7 @@
     function estoqueController(toastApp){
         var self = this;
         self.isShowFiltro = true;
+        self.isSelectCosignacao = true;
         self.listaBuscaProduto = [];
         self.initFormEstoque = initFormEstoque;
         self.operacaoEstoque = '';
@@ -25,6 +26,30 @@
         self.isVisibleGradeModelo = false;
         self.resetFormEstoque = resetFormEstoque;
         self.inserirEntradaEstoque = inserirEntradaEstoque;
+        self.bucarListaCliente = bucarListaCliente;
+        self.selecionarCliente = selecionarCliente;
+        self.consignacao = {};
+        self.listaBuscaCliente = [];
+        self.listaProdutoConsigacao = [];
+        self.montarKit = adicionarKit;
+        self.selecionarProdutoConsignacao = selecionarProdutoConsignacao;
+        self.fecharKit = fecharKit;
+
+        function fecharKit(consignacao){
+            console.log(consignacao);
+        }
+
+        function bucarListaCliente(nome){
+            console.log(nome);
+        }
+
+        function adicionarKit(produto){
+            console.log(produto)
+            self.consignacao.listaProduto.push(produto);
+            console.log(self.consignacao)
+            self.produto = {};
+            self.isSelectCosignacao = !self.isSelectCosignacao;
+        }
 
 
         function inserirEntradaEstoque(estoque){
@@ -34,7 +59,7 @@
 
         function resetFormEstoque(){
             self.Busca = { campo:'name'};
-            self.estoque = { data: new Date()}
+            self.estoque = { data: new Date()};
             self.isShowFiltro = !self.isShowFiltro;
 
         }
@@ -45,12 +70,25 @@
             return
         }
 
+        function selecionarProdutoConsignacao(produto){
+            self.produto = produto;
+            self.isSelectCosignacao = !self.isSelectCosignacao;
+            return
+        }
+
+        function selecionarCliente(cliente){
+            self.consignacao = { Client: cliente, dataSaida: new Date(), listaProduto:[]};
+            self.isShowFiltro = !self.isShowFiltro;
+            initEstoque();
+        }
+
         function bucarListaProdutos(busca){
             if(busca.palavraChave === undefined || busca.palavraChave === ""){
                 toastApp.newmessage("Digite algo no campo Busca.");
                 document.getElementById("palavraChaveBusca").focus();
             }
         }
+
 
         function ordenaProduto(campoOrdencao) {
             self.reverse = (self.campoOrdencao === campoOrdencao) ? !self.reverse : false;
@@ -62,7 +100,7 @@
             switch (operacao) {
                 case "consignacao":
                     self.operacaoEstoque = operacao;
-                    console.log(self.operacaoEstoque);
+                    initConsignacao();
                     break;
                 case "venda":
                     self.operacaoEstoque = operacao;
@@ -75,7 +113,14 @@
             }
         }
 
+        var initConsignacao = function(){
+            for(var i = 1; i < 5; i++){
+                var cliente = {id: i, name: "Maria " +i}
+                self.listaBuscaCliente.push(cliente)
+            }
+        }
         var initEstoque = function(){
+            self.listaBuscaProduto = [];
             for(var i = 1; i < 5; i++){
                 var produto = {id: i, name: "Produto" +i, skuProduto:"S"+i+"k"+i+"U"}
                 self.listaBuscaProduto.push(produto)
