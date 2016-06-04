db7c95263498383dce2be4891801c79e00a96ddc
jesus <vander.vander@gmail.com>
2016-06-04 14:37:43 -0300

    Tela estoque para validar - 04062016 - 02

--- webapp/content/app/movimentacao/estoqueController.js
+++ webapp/content/app/movimentacao/estoqueController.js
@@ -10,21 +10,59 @@
         .controller('estoqueController', estoqueController);
 
 
-    function estoqueController(){
+    function estoqueController(toastApp){
         var self = this;
-        self.isShowFiltro = false;
-        self.listaProdutos = [];
+        self.isShowFiltro = true;
+        self.listaBuscaProduto = [];
         self.initFormEstoque = initFormEstoque;
         self.operacaoEstoque = '';
+        self.bucarListaProdutos = bucarListaProdutos;
+        self.Busca = { campo:'name'};
+        self.estoque = { data: new Date()}
+        self.campoOrdencao = 'nome';
+        self.ordenaProduto = ordenaProduto;
+        self.selecionarProduto = selecionarProduto;
+        self.isVisibleGradeModelo = false;
+        self.resetFormEstoque = resetFormEstoque;
+        self.inserirEntradaEstoque = inserirEntradaEstoque;
 
 
+        function inserirEntradaEstoque(estoque){
+            console.log(estoque);
+        }
+
+
+        function resetFormEstoque(){
+            self.Busca = { campo:'name'};
+            self.estoque = { data: new Date()}
+            self.isShowFiltro = !self.isShowFiltro;
+
+        }
+
+        function selecionarProduto(produto){
+            self.estoque = { Produto: produto, data: new Date() };
+            self.isShowFiltro = !self.isShowFiltro;
+            return
+        }
+
+        function bucarListaProdutos(busca){
+            if(busca.palavraChave === undefined || busca.palavraChave === ""){
+                toastApp.newmessage("Digite algo no campo Busca.");
+                document.getElementById("palavraChaveBusca").focus();
+            }
+        }
+
+        function ordenaProduto(campoOrdencao) {
+            self.reverse = (self.campoOrdencao === campoOrdencao) ? !self.reverse : false;
+            self.campoOrdencao = campoOrdencao;
+
+        }
 
         function initFormEstoque(operacao){
             switch (operacao) {
                 case "consignacao":
                     self.operacaoEstoque = operacao;
                     console.log(self.operacaoEstoque);
-
                     break;
                 case "venda":
                     self.operacaoEstoque = operacao;
@@ -32,11 +70,17 @@
                     break;
                 default:
                     self.operacaoEstoque = operacao;
-                    console.log(self.operacaoEstoque);
+                    initEstoque();
                     break;
             }
         }
 
+        var initEstoque = function(){
+            for(var i = 1; i < 5; i++){
+                var produto = {id: i, name: "Produto" +i, skuProduto:"S"+i+"k"+i+"U"}
+                self.listaBuscaProduto.push(produto)
+            }
+        }
 
 
     }
