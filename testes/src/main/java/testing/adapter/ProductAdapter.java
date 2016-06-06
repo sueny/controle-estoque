8bbe2718500507b1aab7e2caf643c067be2bffe2
dtandrade <dandarathamilys@gmail.com>
2016-06-06 18:55:21 -0300

    Alterações para alinhar com interface de Produto

--- testes/src/main/java/testing/adapter/ProductAdapter.java
+++ testes/src/main/java/testing/adapter/ProductAdapter.java
@@ -1,10 +1,10 @@
-package testing.adapter;
+package product;
 
 public interface ProductAdapter {
 	/**
 	 * This method will close the section
 	 */
-	void closeSession(); 
+	void closeSection(); 
 
 	/** When the user hits "compor" button, this method tries to save the product but it will only
 	 * save the prod if it is a valid product. 
@@ -16,11 +16,29 @@ public interface ProductAdapter {
 	 */
 	boolean trySaveProduct();
 	
+	
+	/** This method will try to delete the product and must return a message of success or failure */
+	boolean tryDeleteProduct();
+	
+	/**
+	 * The product can be deleted if it is a registered product
+	 * @return
+	 */
 	void clickDelete(); 
 	
-	void cancelDeletion();
 	
-	/** This method will delete the product and must return a message for the user */
-	boolean deleteProduct();
+	
+	/** This method will cancel the event that will try to delete the Product*/
+	void cancelDeletion();
+
+	/**
+	 * This method is used to choose the product in the list of registered products
+	 */
+	void clickToChooseProduct();
+
+	/**
+	 * When you finish to inform whatever you want to update you press "Alterar". Here is the method to do it
+	 */
+	void clickUpdate();
 	
 }
