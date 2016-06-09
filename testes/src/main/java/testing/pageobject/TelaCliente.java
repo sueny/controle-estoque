051dc18139c11fba8ab1c29bb51c8931f3a05d82
phaffonso <pedrohaffonso@gmail.com>
2016-06-09 16:52:48 -0300

    Concretização e pacote que estava faltando

--- testes/src/main/java/testing/pageobject/TelaCliente.java
+++ testes/src/main/java/testing/pageobject/TelaCliente.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
-import testing.components.MdSelect;
+import testing.uicomponent.MdSelect;
 import testing.util.DriverProvider;
 import testing.util.Routes;
 import testing.util.Utils;
--- testes/src/main/java/testing/pageobject/TelaModeloProduto.java
+++ testes/src/main/java/testing/pageobject/TelaModeloProduto.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import testing.components.MdSelect;
+import testing.uicomponent.MdSelect;
 import testing.util.DriverProvider;
 import testing.util.Routes;
 import testing.util.Utils;
