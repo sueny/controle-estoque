ca785e4bee6744f1e19c054d9da80eb4d173d0c1
dtandrade <dandarathamilys@gmail.com>
2016-06-09 00:54:49 -0300

    Adapter da Consignação

---
+++ testes/src/main/java/testing/adapter/ConsigAdapter.java
@@ -0,0 +1,27 @@
+package consignment;
+
+public interface ConsigAdapter {
+
+	public void clickConsig();
+
+	public void clickClient();
+
+	public void clickProduct();
+
+	public void clickNewConsig();
+
+	public boolean fillAndValidateDate();
+
+	public boolean fillAndValidadePrice();
+
+	public boolean fillAndValidadeObs();
+
+	public boolean clickCloseKit();
+
+	public void closeSection();
+
+	public boolean fillAndValidateQnt();
+
+	public boolean clickAddBt();
+
+}
