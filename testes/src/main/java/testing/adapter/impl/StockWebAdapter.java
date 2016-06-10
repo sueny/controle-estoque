d983875faaf546c9c76d9eb0bb736c1a0dbf6fe6
dtandrade <dandarathamilys@gmail.com>
2016-06-09 23:05:23 -0300

    Entrada no Estoque

---
+++ testes/src/main/java/testing/adapter/impl/StockWebAdapter.java
@@ -0,0 +1,53 @@
+package stock;
+
+public class StockWebAdapter implements StockAdapter {
+
+	@Override
+	public void clickEntryBt() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clickProduct() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean fillAndValidadeObs() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean fillAndValidadePrice(boolean status) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean fillAndValidadeQnt(boolean status) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clickNewSearchBt() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean clickRegister() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void closeSection() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
