d5424ebcbf6a188c01659b22b7597dd35df46b5a
dtandrade <dandarathamilys@gmail.com>
2016-06-10 10:35:35 -0300

    Consignação - estados

---
+++ testes/src/main/java/testing/stateMachine/ConsigState.java
@@ -0,0 +1,15 @@
+package consignment;
+
+public enum ConsigState {
+	Idle,
+	FindClient,
+	AddProducts,
+	FillFormProduct,
+	Adding,
+	ProdAdded,
+	ProdNotAdded,
+	FinalInfo,
+	KitReady,
+	FilledForm
+
+}
