cfde7c362d9223331a3731aaa439da03def0b3cf
jesus <vander.vander@gmail.com>
2016-06-12 15:28:08 -0300

    Alterações - 11062016 01

--- webapp/content/app/app-config.js
+++ webapp/content/app/app-config.js
@@ -21,9 +21,9 @@
     function appConfig($mdThemingProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('purple', {
-                'default': 'A100', // by default use shade 400 from the pink palette for primary intentions
-                'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
-                'hue-2': '200', // use shade 600 for the <code>md-hue-2</code> class
+                'default': '600', // by default use shade 400 from the pink palette for primary intentions
+                'hue-1': '500', // use shade 100 for the <code>md-hue-1</code> class
+                'hue-2': '400', // use shade 600 for the <code>md-hue-2</code> class
                 'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
             })
             // If you specify less than all of the keys, it will inherit from the
